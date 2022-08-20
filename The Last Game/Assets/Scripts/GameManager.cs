a0a6d21c93107a82df79153411113c4ca8e3d4ec
songwooseung <110340919+songwooseung@users.noreply.github.com>
2022-08-20 20:24:21 +0900

    mainmanager, 다른씬 저장불러오기 버튼, 위치 고정

--- The Last Game/Assets/Scripts/GameManager.cs
+++ The Last Game/Assets/Scripts/GameManager.cs
@@ -12,26 +12,70 @@ public class GameManager : MonoBehaviour
     //public QuestManager questManager;
     public GameObject scanObject;
     public GameObject player;
-    public GameObject menuSet;
+    public GameObject menuSet; 
 
     //Shop
     public int Coin;
     public int[] itemsCount;
 
-     void Start()
+
+    void Start()
      {
          GameLoad();
-         //questText.text = questManager.CheckQuest();
-     }
+        //questText.text = questManager.CheckQuest();
+
+        if (SceneManager.GetActiveScene().name == "Buk Mun")
+        {
+            float x = 0;
+            float y = -6.5f;
+            player.transform.position = new Vector3(x, y, 0);
+
+        }
+        else if (SceneManager.GetActiveScene().name == "Jeong Mun")
+        {
+            float x = 0;
+            float y = 7f;
+            player.transform.position = new Vector3(x, y, 0);
+
+        }
+        else if (SceneManager.GetActiveScene().name == "Seo Mun")
+        {
+            float x = 9.5f;
+            float y = 0f;
+            player.transform.position = new Vector3(x, y, 0);
+
+        }
+        else if (SceneManager.GetActiveScene().name == "Dong Mun")
+        {
+            float x = -10f;
+            float y = 0f;
+            player.transform.position = new Vector3(x, y, 0);
+
+        }
 
 
-    
+    }
 
-     public void GameSave()
+    private void Update()
+    {
+        //Sub Menu
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuSet.activeSelf)
+                menuSet.SetActive(false);
+            else
+                menuSet.SetActive(true);
+        }
+
+    }
+
+
+
+    public void GameSave()
      {
          PlayerPrefs.SetFloat("PlayerX",player.transform.position.x);
          PlayerPrefs.SetFloat("PlayerY",player.transform.position.y);
-         PlayerPrefs.SetInt("Coin", Coin);
+         PlayerPrefs.SetInt("Coin",Coin);
         //  PlayerPrefs.SetInt("QustId",questManager.questId);
         //  PlayerPrefs.SetInt("QustActionIndex",questManager.questActionIndex);
          PlayerPrefs.Save();
@@ -61,15 +105,34 @@ public class GameManager : MonoBehaviour
 
      public void GameReset()
      {
+        if (SceneManager.GetActiveScene().name == "Buk Mun")
+        {
+            SceneManager.LoadScene("Main Map");
+        }
+        if (SceneManager.GetActiveScene().name == "Jeong Mun")
+        {
+            SceneManager.LoadScene("Main Map");
+        }
+        if (SceneManager.GetActiveScene().name == "Dong Mun")
+        {
+            SceneManager.LoadScene("Main Map");
+        }
+        if (SceneManager.GetActiveScene().name == "Seo Mun")
+        {
+            SceneManager.LoadScene("Main Map");
+        }
          float x = 0;
-         float y = 0;
+         float y = -3.0f;
          //int questId = 0;
          int coin = 0;
          //int questActionIndex = 0;
          player.transform.position = new Vector3(x,y,0);
+        
         //  questManager.questId = questId;
         //  questManager.questActionIndex = questActionIndex;
         //  questManager.ControlObject();
-         Time.timeScale = 1;
+        Time.timeScale = 1;
      }
+
 }
+
